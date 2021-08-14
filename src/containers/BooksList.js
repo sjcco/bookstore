ef63004ad854f4242d65af803ab593c35fdff913
sjcco <73748496+sjcco@users.noreply.github.com>
2021-08-14 14:45:37 -0500

    Refactor to use mapStateToProps

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,23 +1,32 @@
-import { useSelector } from 'react-redux';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Book from '../components/Book';
 
-const BookList = () => {
-  const books = useSelector(state => state.books);
+const BookList = ({ books }) => (
+  <table>
+    <thead>
+      <tr>
+        <th>book ID</th>
+        <th>Title</th>
+        <th>Category</th>
+      </tr>
+    </thead>
+    <tbody>
+      {books.map(book => (<Book key={book.id} book={book} />))}
+    </tbody>
+  </table>
+);
 
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>book ID</th>
-          <th>Title</th>
-          <th>Category</th>
-        </tr>
-      </thead>
-      <tbody>
-        {books.map(book => (<Book key={book.id} book={book} />))}
-      </tbody>
-    </table>
-  );
+BookList.propTypes = {
+  books: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+  })).isRequired,
 };
 
-export default BookList;
+const mapStateToProps = state => ({
+  books: state.books,
+});
+
+export default connect(mapStateToProps)(BookList);
