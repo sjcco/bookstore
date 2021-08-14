4d0d854eb9845514c60b25ab73b76c94b4efc974
sjcco <73748496+sjcco@users.noreply.github.com>
2021-08-14 04:13:58 -0500

    Define Containers

---
+++ src/containers/BooksForm.js
@@ -0,0 +1,21 @@
+const BooksForm = () => {
+  const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+
+  return (
+    <form>
+      <label htmlFor="title">
+        Title
+        <input type="text" id="title" />
+      </label>
+      <label htmlFor="category">
+        Category
+        <select name="category" id="category">
+          {categories.map(category => <option key={category} value={category}>{category}</option>)}
+        </select>
+      </label>
+
+    </form>
+  );
+};
+
+export default BooksForm;
---
+++ src/containers/BooksList.js
@@ -0,0 +1,23 @@
+import { useSelector } from 'react-redux';
+import Book from '../components/Book';
+
+const BookList = () => {
+  const books = useSelector(state => state.books);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>book ID</th>
+          <th>Title</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        {books.map(book => (<Book key={book.id} book={book} />))}
+      </tbody>
+    </table>
+  );
+};
+
+export default BookList;
