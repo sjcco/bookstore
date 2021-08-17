8d39aa27c5a63f1745a995549ace63caf369f0ba
sjcco <73748496+sjcco@users.noreply.github.com>
2021-08-17 01:40:30 -0500

    Define filter function

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,13 @@
+const filterByCategory = state => {
+  const { books, categoryFilter } = state;
+  if (categoryFilter === 'All') {
+    return books;
+  }
+  return books.filter(book => book.category === categoryFilter);
+};
+
 const uniqueId = () => Math.floor(Math.random() * 9000000000) + 1;
 
-export default uniqueId;
+const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+
+export { uniqueId, categories, filterByCategory };
